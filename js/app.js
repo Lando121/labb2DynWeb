c33b159a60fc8ab0652690b058faf4db4a5274fc
Aroshine <aroshine@kth.se>
2016-02-11 15:05:39 +0100

    ej klar

--- js/app.js
+++ js/app.js
@@ -1,23 +1,26 @@
 $(function() {
 	//We instantiate our model
 	var model = new DinnerModel();
+	var viewCache; 
 
 	//And create the needed controllers and views
 	//var exampleView = new ExampleView($("#exampleView"));
 	//exampleView.numberOfGuests.html(model.getNumberOfGuests());
 	//exampleView.totalCost.html(model.getTotalMenuPrice());
 
-showScreens();
+	showScreens(1);
 
 });
 
 // skapar en funktion som displayar screen
+
 function showScreens(id){
 	// Gör en AJAX-request som hämtar t.ex. screen1.html
-	$.ajax({url:"screen1.html", success: function(result){
-        
-    }});
-
+	$.ajax({url:'views.html',success: function(views) {
+		$.ajax({url:"screen"+id+".html", success: function(screen){
+        	console.log(screen);
+    	}});
+	}});
 }
 
 
