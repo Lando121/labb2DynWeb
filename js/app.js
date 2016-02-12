35b0321cd4b7a9b7d74749508774b8a26d7545d5
Aroshine <aroshine@kth.se>
2016-02-12 19:00:15 +0100

    kopierat in allt igen

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ $(function() {
 	//exampleView.numberOfGuests.html(model.getNumberOfGuests());
 	//exampleView.totalCost.html(model.getTotalMenuPrice());
 
-	showScreens(1); //visar screen1
+	showScreens(1);
 
 	$.ajaxSetup ({
 	    // Disable caching of AJAX responses
@@ -21,21 +21,18 @@ $(function() {
 
 function showScreens(id){
 	// Gör en AJAX-request som hämtar t.ex. screen1.html
-	// 1. hämtar in alla views
 	$.ajax({url:'views.html',success: function(views) {
 		views = $(views);
-	// 2. hämtar det screen vi vill komma åt ex. 1
 		$.ajax({url:"screens/screen"+id+".html", success: function(screenHtml){
 			var screen = $(screenHtml);
-	//3. går igenom varje viewtag som finns i screen-filen
 			screen.each(function(){
         		for(var i=0; i < views.length; i++) {
         			if(views[i].id == this.id) {
-        				$(this).html(views[i].innerHTML); //innerHTML hämtar all html som finns i views-taggen från view.html
+        				$(this).html(views[i].innerHTML); //innerHTML hämtar all html som finns i views-taggen
         			}
         		}
         	});
-        	$("#viewsContainer").html(screen); //viewscontainer finns i index.html
+        	$("#viewsContainer").html(screen);
     	}});
 	}});
 }
