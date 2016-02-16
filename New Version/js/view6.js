39ea901b92f821ae9430b8dc2af99191616bb5ed
Aroshine <aroshine@kth.se>
2016-02-16 17:03:31 +0100

    view 6 done

--- New Version/js/view6.js
+++ New Version/js/view6.js
@@ -1,7 +1,18 @@
 //$.getScript("js/view3.js");
-var view3Func = function (container){
-
+var view3Func = function (container, model){
+	//toast
+	this.View6 = container.find("#view6_toast");
+	this.View6.html(model.getDishById(1).name);
+	this.View6 = container.find("#toast_description");
+	this.View6.html(model.getDishById(1).description);
 	
+
+	//sugardough
+	this.View6 = container.find("#view6_sour");
+	this.View6.html(model.getDishById(2).name);
+	this.View6 = container.find("#sour_description");
+	this.View6.html(model.getDishById(2).description);
+
 }
 $(function() {
 	//We instantiate our model
@@ -16,5 +27,5 @@ $(function() {
 	//exampleView.numberOfGuests.html(model.getNumberOfGuests());
 	//exampleView.totalCost.html(model.getTotalMenuPrice());
 
-	var view3 = new view3Func($("#View6"));
+	var view3 = new view3Func($("#View6"), model);
 });
