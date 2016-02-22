5cae0563be635eafe4c4a2efc0aa8914aa7bb5df
Aroshine <aroshine@kth.se>
2016-02-22 17:33:28 +0100

    confirm dinner done

--- labb3(maybe)/js/controllers/view2Controller.js
+++ labb3(maybe)/js/controllers/view2Controller.js
@@ -5,5 +5,16 @@ var view2Controller = function(view, model) {
  		model.setNumberOfGuests($("#peopleInput").val());
 
  	});
+ 	console.log(model.getFullMenu());
+ 	if(model.getFullMenu().length > 0){
+ 		$('#confirmButton').click(function() {
+    		$("#View2_after").hide();
+    		$("#View3").hide();
+    		$("#View5").show();
+    	
+});
+
+ 	}
+
 
 }
