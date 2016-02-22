73b56620a9af81b90900c3ad56ec1a301d30181e
Lando121 <emilio.lando@gmail.com>
2016-02-22 14:49:39 +0100

    mapp

---
+++ labb3(maybe)/js/controllers/controller3.js
@@ -0,0 +1,16 @@
+var ViewController = function(view, model ) {
+ 
+ 	$("#peopleInput").val(numGuest);
+
+
+
+
+
+ /** view.plusButton.click(function(){
+ model.setNumberOfGuests(model.getNumberOfGuests() + 1);
+ });
+ 
+ view.minusButton.click(function(){
+ model.setNumberOfGuests(model.getNumberOfGuests() - 1);
+ }); **/
+}
