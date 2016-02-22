02e5d5b2330c472c46a74fd6be4198e17a84e18f
Lando121 <emilio.lando@gmail.com>
2016-02-22 18:06:48 +0100

    view3 controller, search-functionality

--- labb3(maybe)/js/controllers/view3Controller.js
+++ labb3(maybe)/js/controllers/view3Controller.js
@@ -6,4 +6,8 @@ var view3Controller = function(view, model){
 		view.load();
 	});
 
+	$("#view3_search_button").on("click", function(){
+		view.load();	
+	});
+
 }
--- labb3(maybe)/js/view3.js
+++ labb3(maybe)/js/view3.js
@@ -2,14 +2,33 @@ var view3Func = function (container,model){
 
 	
 
-	var showing;	
+		
 
 	this.load = function(){
-		showing = model.getTypeDish($("#view3_dropdown_items option:selected").text());
-		console.log($("#view3_dropdown_items option:selected").text());
+		var showing = model.getTypeDish($("#view3_dropdown_items option:selected").text());
+		var text = $("#view3_search_text_field").val().toLowerCase().trim();
 		$("#view3_dish_row").html("");
+		if(text != ""){
+			for(k = 0; k < showing.length; k++){
+
+				var bool = false;
+				var curWords = showing[k].name.split(" ");
+				for(p = 0; p < curWords.length; p++){
+					if(text == curWords[p].toLowerCase()){
+						bool = true;
+					}
+				}
+				if(!bool){
+					showing.splice(k, 1);
+					k--;
+				}
+
+			}
+		} 
 		for(i = 0; i<showing.length; i++){
-			$("#view3_dish_row").append("<div class='col-sm-12 col-sm-2 view3_dish_col'><div class='row view3_dish_obj'><img src='images/" + showing[i].image +  "' class='view3_dish_img'></div>" + "<div class='row view3_dish_obj view3_dish_name'>" + showing[i].name + "</div>"+"<div class='row view3_dish_info'>" + showing[i].description + "</div> </div>");
+			
+				$("#view3_dish_row").append("<div class='col-sm-12 col-sm-2 view3_dish_col'><div class='row view3_dish_obj'><img src='images/" + showing[i].image +  "' class='view3_dish_img'></div>" + "<div class='row view3_dish_obj view3_dish_name'>" + showing[i].name + "</div>"+"<div class='row view3_dish_info'>" + showing[i].description + "</div> </div>");	
+			
 			
 		}	
 
