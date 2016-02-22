66e2b410ab88f1345485547223638ee8652bd354
Aroshine <aroshine@kth.se>
2016-02-22 17:51:41 +0100

    gobackdinner button done

---
+++ labb3(maybe)/js/controllers/view4Controller.js
@@ -0,0 +1,9 @@
+var view4Controller = function(view, model) {
+ 		$('#goBack').click(function() {
+    		$("#View5").hide();
+    		$("#View2_after").show();
+    		$("#View3").show();
+    	
+});
+
+ 	}
