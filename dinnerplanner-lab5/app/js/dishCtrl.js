955cb920665383b2c2b134fa5f6aa802e717d251
lando121 <emilio.lando@gmail.com>
2016-03-08 12:37:30 +0100

    småsaker, view4

--- dinnerplanner-lab5/app/js/dishCtrl.js
+++ dinnerplanner-lab5/app/js/dishCtrl.js
@@ -1,11 +1,21 @@
 // Dinner controller that we use whenever we want to display detailed
 // information for one dish
 dinnerPlannerApp.controller('DishCtrl', function ($scope,$routeParams,Dinner) {
-  
-  $scope.getDishPrice = function() {
-  	var obj = Dinner.getDish($routingParams.dishId);
-  	return Dinner.getDishPrice(obj);
+
+	$scope.getDish = function(){
+		//TODO	
+	}
+
+	$scope.getNumberOfGuests = function() {
+    return Dinner.getNumberOfGuests();
   }
+  
+  	$scope.getDishPrice = function() {
+  		var obj = Dinner.getDish($routingParams.dishId);
+  		return Dinner.getDishPrice(obj);
+  	}
+
+
   // TODO in Lab 5: you need to get the dish according to the routing parameter
   // $routingParams.paramName
   // Check the app.js to figure out what is the paramName in this case
